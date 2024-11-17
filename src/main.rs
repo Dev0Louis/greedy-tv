5f3de06d43df46d7fff9b995dd859a6a32f0e748
Dev0Louis <Dev0Louis>
2024-11-17 20:27:11 +0100

    It works?

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ fn main() {
     });
 
     let now = Instant::now();
-    let screen: Box<Mutex<&ListScreen>> = Box::new(Mutex::new(get_main_screen()));
+    let screen: Box<Mutex<Box<dyn Screen>>> = Box::new(Mutex::new(Box::new(ListScreen {})));
     let mut terminal = ratatui::init();
     loop {
         terminal.draw(|frame| {draw(frame, now.elapsed().as_secs(), &screen)}).expect("failed to draw frame");
@@ -38,24 +38,15 @@ fn main() {
             let event = event::read().expect("failed to read event");
             match event {
                 Event::Key(key_event) => {
-                    if (key_event.code == KeyCode::Up) {
-                        let discoveries_index = &mut get_discoveries_index();
-                        let mut lock = discoveries_index.lock().unwrap();
-                        if (*lock != 0) {
-                            (*lock) -= 1;
+                    let mut screen = screen.lock().unwrap();
+                    match screen.on_key(key_event) {
+                        Some(new_screen) => {
+                            let a: Box<dyn 'static + Screen> = new_screen;
+                            *screen = a;
                         }
-                    } else if (key_event.code == KeyCode::Down) {
-                        let discoveries_index = &mut get_discoveries_index();
-                        let mut lock = discoveries_index.lock().unwrap();
-                        if (is_in_discoveries_range((*lock).clone() + 1)) {
-                            (*lock) += 1;
-                        }
-                    } else if key_event.code == KeyCode::Enter {
-                        let discoveries_index = &mut get_discoveries_index();
-                        let index = *discoveries_index.lock().unwrap();
-                        let out: &ServiceDiscovery = get_discoveries().lock().unwrap().get(index as usize).unwrap();
-
-                    }
+                        None => {}
+                    };
+                    
                 }
                 _ => {}
             }
@@ -65,7 +56,7 @@ fn main() {
     ratatui::restore();
 }
 
-fn draw(frame: &mut Frame, seconds: u64, screen: &Mutex<&ListScreen>) {
+fn draw(frame: &mut Frame, seconds: u64, screen: &Mutex<Box<dyn Screen>>) {
     let mut lines: Vec<Line> = Vec::new();
     let symbol = {
         if seconds % 2 == 0 {
@@ -79,69 +70,19 @@ fn draw(frame: &mut Frame, seconds: u64, screen: &Mutex<&ListScreen>) {
 
     let screen = screen.lock().unwrap();
     screen.draw(&mut lines, seconds);
-
-
-
-    get_discoveries().lock().unwrap().iter().enumerate().for_each(|(index, service)| {
-        let selection_indicator = get_symbol_for_index(index);
-
-        let line: Span = {
-            let line1 = Span::from(
-                format!(" {}, IP: {}, Port: {}",
-                        service.name(),
-                        service.address(),
-                        service.port()
-                )
-            );
-            if (index % 2 == 0) {
-                line1.style(Style::from(Color::Cyan))
-            } else {
-                line1.style(Style::from(Color::LightCyan))
-            }
-        };
-
-        lines.push(
-            Line::from(vec![selection_indicator, line])
-        );
-    });
-
+    
     let text = Text::from(lines);
     frame.render_widget(text, frame.area());
 }
 
-fn get_symbol_for_index(index: usize) -> Span<'static> {
-    if (is_selected(&index)) {
-        Span::from("[x]").style(Style::from(Color::White))
-    } else {
-        Span::from("[ ]").style(Style::from(Color::Gray))
-    }
-}
 
-fn is_selected(index: &usize) -> bool {
-    index.eq(&(*get_discoveries_index().lock().unwrap() as usize))
-}
-
-fn is_in_discoveries_range(index: u8) -> bool {
-    let discoveries = &*get_discoveries().lock().unwrap();
-    discoveries.len() > index as usize
-}
 
 pub fn get_discoveries() -> &'static Mutex<Vec<ServiceDiscovery>> {
-    &get_main_screen().discoveries
+    static DISCOVERIES: Mutex<Vec<ServiceDiscovery>> = Mutex::new(Vec::<ServiceDiscovery>::new());
+    &DISCOVERIES
 }
 
-pub fn get_discoveries_index() -> &'static Mutex<u8> {
-    get_main_screen().index
-}
-
-pub fn set_discoveries_index(newIndex: usize) {
-    get_main_screen().index = newIndex
-}
-
-pub fn get_main_screen() -> &'static mut ListScreen {
-    static mut MAIN_SCREEN: ListScreen = ListScreen {
-        discoveries: Mutex::new(Vec::<ServiceDiscovery>::new()),
-        index: 0
-    };
-    &mut MAIN_SCREEN
+pub fn get_discoveries_index() -> &'static Mutex<usize> {
+    static INDEX: Mutex<usize> = Mutex::new(0);
+    &INDEX
 }
--- src/screen.rs
+++ src/screen.rs
@@ -1,24 +1,155 @@
+use std::fmt::format;
+use std::ops::Add;
 use std::sync::Mutex;
+use crossterm::event::{KeyCode, KeyEvent};
+use crossterm::style::style;
 use ratatui::Frame;
+use ratatui::layout::Spacing;
+use ratatui::prelude::{Color, Span, Style};
+use ratatui::style::{Styled, Stylize};
 use ratatui::text::Line;
-use zeroconf::ServiceDiscovery;
+use zeroconf::{ServiceDiscovery, TxtRecord};
+use zeroconf::txt_record::TTxtRecord;
+use crate::{get_discoveries, get_discoveries_index};
 
 pub trait Screen {
-    fn draw(&self, frame: &mut Vec<Line>, seconds_since_start: u64);
-    fn on_key(&mut self);
+    fn draw<'a>(&'a self, lines: &mut Vec<Line<'a>>, seconds_since_start: u64);
+    fn on_key(&self, key_event: KeyEvent) -> Option<Box<dyn Screen>>;
 }
 
+pub struct ViewDiscoveryScreen {
+    pub(crate) discovery: ServiceDiscovery
+}
+
+impl ViewDiscoveryScreen {
+    fn style_span(string: String, alternative: bool) -> Span<'static> {
+        if alternative {
+            Span::from(string).style(Style::from(Color::White).bold())
+        } else {
+            Span::from(string).style(Style::from(Color::White).not_bold())
+        }
+    }
+}
+
+impl Screen for ViewDiscoveryScreen {
+    fn draw<'a>(&'a self, lines: &mut Vec<Line<'a>>, seconds_since_start: u64) {
+        let name = self.discovery.name().clone();
+        let hostname = self.discovery.host_name().clone().add(":").add(&self.discovery.port().to_string());
+        let ip = self.discovery.domain().clone().add(":").add(&self.discovery.port().to_string());
+        let mut grey = false;
+        lines.push(Line::from("Name:").style(Style::from(Color::Blue).bg(Color::Black)));
+        lines.push(Line::from(Self::style_span(name, grey))); grey = !grey;
+        lines.push(Line::from("Hostname:").style(Style::from(Color::Blue).bg(Color::Black)));
+        lines.push(Line::from(Self::style_span(hostname, grey))); grey = !grey;
+        lines.push(Line::from("IP + Port:").style(Style::from(Color::Blue).bg(Color::Black)));
+        lines.push(Line::from(Self::style_span(ip, grey))); grey = !grey;
+
+        lines.push(Line::from("Service:").style(Style::from(Color::Blue).bg(Color::Black)));
+        
+        let service = self.discovery.service_type();
+        lines.push(Line::from(Self::style_span(format!("[{},{}]", service.name(), service.protocol()), grey))); grey = !grey;
+        lines.push(Line::from("Service Subtype:").style(Style::from(Color::Blue).bg(Color::Black)));
+        
+        
+        let sub_types = service.sub_types();
+        if (sub_types.is_empty()) {
+            lines.push(Line::from(Self::style_span("No sub types.".to_string(), grey))); grey = !grey;
+        } else {
+            sub_types.iter().for_each(|t| lines.push(Line::from(t.clone())));
+        }
+        
+        match self.discovery.txt() {
+            None => {}
+            Some(txt) => {
+                txt.iter().for_each(|t| {
+                    lines.push(Line::from(format!("{} | {}", t.0, t.1)));
+                })
+            }
+        };
+  
+    }
+    
+    fn on_key(&self, key_event: KeyEvent) -> Option<Box<dyn Screen>> {
+        if (key_event.code == KeyCode::Esc) {
+            Some(Box::from(ListScreen {}))
+        } else {
+            None
+        }
+    }
+}
+
+
 pub struct ListScreen {
-    pub(crate) discoveries: Mutex<Vec<ServiceDiscovery>>,
-    pub(crate) index: Mutex<usize>
+    
 }
 
 impl Screen for ListScreen {
     fn draw(&self, lines: &mut Vec<Line>, seconds_since_start: u64) {
-        lines.push(Line::from("THIS WORKS!"))
+        get_discoveries().lock().unwrap().iter().enumerate().for_each(|(index, service)| {
+            let selection_indicator = get_symbol_for_index(index);
+
+            let line: Span = {
+
+                let span = Span::from(
+                    format!(" {}, Hostname: {}, IP: {}, Port: {}",
+                            service.name(),
+                            service.host_name(),
+                            service.address(),
+                            service.port()
+                    )
+                );
+                if (index % 2 == 0) {
+                    span.style(Style::from(Color::Cyan))
+                } else {
+                    span.style(Style::from(Color::LightCyan))
+                }
+            };
+
+            lines.push(
+                Line::from(vec![selection_indicator, line])
+            );
+        });
     }
 
-    fn on_key(&mut self) {
-        todo!()
+    fn on_key(&self, key_event: KeyEvent) -> Option<Box<dyn 'static + Screen>> {
+        if (key_event.code == KeyCode::Up) {
+            let discoveries_index = &mut get_discoveries_index();
+            let mut lock = discoveries_index.lock().unwrap();
+            if (*lock != 0) {
+                (*lock) -= 1;
+            }
+        } else if (key_event.code == KeyCode::Down) {
+            let discoveries_index = &mut get_discoveries_index();
+            let mut lock = discoveries_index.lock().unwrap();
+            if (is_in_discoveries_range((*lock).clone() + 1)) {
+                (*lock) += 1;
+            }
+        } else if key_event.code == KeyCode::Enter {
+            let discoveries_index = &mut get_discoveries_index();
+            let index = *discoveries_index.lock().unwrap();
+            let out: ServiceDiscovery = get_discoveries().lock().unwrap().get(index).unwrap().clone();
+            return Some(Box::new(
+                ViewDiscoveryScreen {
+                    discovery: out
+                }));
+        }
+        None
     }
+}
+
+fn get_symbol_for_index(index: usize) -> Span<'static> {
+    if (is_selected(&index)) {
+        Span::from("[x]").style(Style::from(Color::White))
+    } else {
+        Span::from("[ ]").style(Style::from(Color::Gray))
+    }
+}
+
+fn is_selected(index: &usize) -> bool {
+    index.eq(&(*get_discoveries_index().lock().unwrap() as usize))
+}
+
+fn is_in_discoveries_range(index: usize) -> bool {
+    let discoveries = &*get_discoveries().lock().unwrap();
+    discoveries.len() > index
 }
